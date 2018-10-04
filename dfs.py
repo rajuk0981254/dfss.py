4fc0809122d9b90118d359b13a2c7d2d9cdc3992
rajuk0981254 <43857560+rajuk0981254@users.noreply.github.com>
2018-10-04 22:10:02 +0530

    added __init__(self) method.

--- dfs.py
+++ dfs.py
@@ -7,7 +7,11 @@ from collections import defaultdict
 # adjacency list representation 
 class Graph: 
   
-   
+     # Constructor 
+    def __init__(self): 
+  
+        # default dictionary to store graph 
+        self.graph = defaultdict(list) 
   
     # function to add an edge to graph 
     def addEdge(self,u,v): 
